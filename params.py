3d3089bb5355954940d53a2dd438715b36f57923
Jasen Jackson <jasenjackson97@gmail.com>
2018-04-10 18:00:16 -0500

    Add files via upload

---
+++ params.py
@@ -0,0 +1,15 @@
+## file output
+outdir = '/Users/jasenmatthewjackson/github/Comp271/Q-learning/results7'
+extension = "Q-learning-"
+
+##Q-table initialization parameters
+n_vertical = 17
+n_horizontal = 29
+
+## Algorithm Hyper-params
+episode_count = 4000000000000
+eps = 0 #epsilon = exploration factor
+gamma = 1
+alpha = 0.618
+initial_lr = 0.618 #initial alpha
+min_lr = 0.618 #minimum alpha
